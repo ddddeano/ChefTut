ae0aae94e7f4989de444cf4336a8b4be6cd88b29
ddddeano <55237431+ddddeano@users.noreply.github.com>
2020-09-03 13:56:25 +0200

    Add files via upload
    Lesson 1 updated.
    
    I had set entity name to Purchases when it should be singular Purchase - whist fixing i also noticed i had not set the constraints on .name for Supplier and Ingredient - which ties in with the Merge Policy

---
+++ Chef/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Chef
+//
+//  Created by Daniel Watson on 03/09/2020.
+//  Copyright © 2020 Daniel Watson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
